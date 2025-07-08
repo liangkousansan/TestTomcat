99b13519b0a84e60a7fb9c44077e5c2498c76e38
liuxinli <liuxinli03@kuaishou.com>
2025-07-08 11:32:20 +0800

    1.0 tomcat

---
+++ src/main/java/org/example/HelloServlet.java
@@ -0,0 +1,22 @@
+package org.example;
+
+/**
+ * @author yourname <liuxinli03@kuaishou.com>
+ * Created on 2025-07-08
+ */
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Hello, Tomcat!</h1>");
+    }
+}
